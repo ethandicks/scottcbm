df7f8c3402c2d9449280b9062c0b52bee6467bc1
Ethan Dicks <ethan.dicks@gmail.com>
2021-10-06 12:30:12 -0400

    Add several additional debug prints to trace logic flow problem

--- debuglb.asm
+++ debuglb.asm
@@ -25,6 +25,11 @@ RMDMSG .BYTE "ROOM=",0
 CMDMSG .BYTE "CMD='",0
 NVMSG  .BYTE "NV='",0
 VMMSG  .BYTE "MATCH='",0
+TSFMSG .BYTE "Test bit ",0
+PFMSG  .BYTE "F1/F2 = ",0
+MNMSG  .BYTE "MSG #",0
+TNMSG  .BYTE "TEST #",0
+ANMSG  .BYTE "ACTION #",0
 
 MSGEND .BYTE "'",CR,0
 
@@ -73,6 +78,72 @@ DB_EDD
  RTS
 ;
 
+; Print message number to be printed
+DB_MSGN
+ STA VTMP
+ RSAVE
+ PRINT MNMSG
+ LDA VTMP
+ JSR DPRINT
+ JSR CRLF
+ RREST
+ RTS
+;
+
+; Print test routine to be executed
+DB_TSTN
+ STA VTMP
+ RSAVE
+ PRINT TNMSG
+ LDA VTMP
+ JSR DPRINT
+ JSR CRLF
+ RREST
+ RTS
+;
+
+; Print action routine to be executed
+DB_ACTN
+ STA VTMP
+ RSAVE
+ PRINT ANMSG
+ LDA VTMP
+ JSR DPRINT
+ JSR CRLF
+ RREST
+ RTS
+;
+
+; Print out info from testing SF bits
+DB_TSTSF
+ RSAVE
+ PRINT TSFMSG
+ LDA SF
+ JSR DB_PHEX
+ LDA SF+1
+ JSR DB_PHEX
+ LDA LL
+ JSR DPRINT
+ JSR CRLF
+ RREST
+ RTS
+;
+
+; Print out state of F1 and F2 flags
+DB_PF12
+ RSAVE
+ PRINT PFMSG
+ LDA F1
+ JSR DB_PHEX
+ LDA #$20
+ JSR OUTCH
+ LDA F2
+ JSR DB_PHEX
+ JSR CRLF
+ RREST
+ RTS
+
+; Print out matching rule number (daemons and player command)
 DB_VMAT
  RSAVE
  PRINT VMMSG
